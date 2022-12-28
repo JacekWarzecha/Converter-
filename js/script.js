90b62e34ce926743a764a1acc9ee3ed6334873ff
JacekWarzecha <drjackys@gmail.com>
2022-12-28 16:01:29 +0100

    zmiana tła, ikony, OG, const

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-let quotesElement = document.querySelector(".js-quotes");
+{
+  const quotesElement = document.querySelector(".js-quotes");
 
-let exchangeElement = document.querySelector(".js-exchange");
+  const exchangeElement = document.querySelector(".js-exchange");
 
-let formCurrency = document.querySelector(".js-form");
+  const formCurrency = document.querySelector(".js-form");
 
-let costElement = document.querySelector(".js-cost");
+  const costElement = document.querySelector(".js-cost");
 
-formCurrency.addEventListener("input", () => {
-  let quotes = quotesElement.value;
-  let exchange = exchangeElement.value;
-  let cost = quotes * exchange;
+  formCurrency.addEventListener("input", () => {
+    const quotes = quotesElement.value;
+    const exchange = exchangeElement.value;
+    let cost = quotes * exchange;
 
-  costElement.innerText = cost.toFixed(2);
+    costElement.innerText = cost.toFixed(2);
 
-  if (cost <= 0) {
-    costElement.innerText = "Wprowadź dodatnie liczby";
-  }
-});
+    if (cost <= 0) {
+      costElement.innerText = "Wprowadź dodatnie liczby";
+    }
+  });
+}
