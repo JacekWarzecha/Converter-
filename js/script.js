54d06d2c211b0002ebf4a156814cb005c67a8cb4
JacekWarzecha <drjackys@gmail.com>
2022-12-29 14:50:32 +0100

    styling and added functions

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 {
-  const quotesElement = document.querySelector(".js-quotes");
-
-  const exchangeElement = document.querySelector(".js-exchange");
-
-  const formCurrency = document.querySelector(".js-form");
-
-  const costElement = document.querySelector(".js-cost");
-
-  formCurrency.addEventListener("input", () => {
+  const priceCalculate = () => {
+    const quotesElement = document.querySelector(".js-quotes");
+    const exchangeElement = document.querySelector(".js-exchange");
+    const costElement = document.querySelector(".js-cost");
     const quotes = quotesElement.value;
     const exchange = exchangeElement.value;
     let cost = quotes * exchange;
-
     costElement.innerText = cost.toFixed(2);
 
     if (cost <= 0) {
       costElement.innerText = "Wprowadź dodatnie liczby";
     }
-  });
+  };
+
+  // Chciałem zrobić funkcję i wywołać ją po przecinku za .."input", priceCalculate, /tu\ -ale nie działało
+
+  // const textShow = () => {
+  //   if (cost <= 0) {
+  //     costElement.innerText = "Wprowadź dodatnie liczby";
+  //   }
+  // }
+
+  const priceShow = () => {
+    const formCurrency = document.querySelector(".js-form");
+    formCurrency.addEventListener("input", priceCalculate);
+  };
+
+  priceShow();
 }
